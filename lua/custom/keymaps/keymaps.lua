fe723aad50a6d66d191c6977c3a2a51984c93a67
John Hooper <nisse038@gmail.com>
2025-03-25 17:59:00 -0500

    add more keymaps for the text case stuff

--- lua/custom/keymaps/keymaps.lua
+++ lua/custom/keymaps/keymaps.lua
@@ -15,3 +15,30 @@ vim.keymap.set('n', '<leader>cb', '<cmd>%bd|e#|bd# <CR>', { desc = '[c]lose all 
 vim.keymap.set('n', 'F', vim.diagnostic.open_float, { desc = 'open [F]loat diagnostics' })
 
 vim.keymap.set('n', '-', '<CMD>Oil --float <CR>', { desc = 'Open parent directory in oil' })
+
+-- Common function to set up mappings for each case
+local function setup_textcase_keymaps(key, case, desc, op_desc)
+  -- Normal mode: Convert current word
+  vim.keymap.set('n', 'ga' .. key, function()
+    require('textcase').current_word(case)
+  end, { noremap = true, silent = true, desc = 'Convert to ' .. desc })
+  -- Normal mode: LSP rename
+  vim.keymap.set('n', 'ga' .. key:upper(), function()
+    require('textcase').current_word(case)
+  end, { noremap = true, silent = true, desc = 'LSP rename to ' .. desc })
+  -- Normal mode: Operator
+  vim.keymap.set('n', 'gao' .. key, function()
+    require('textcase').operator(case)
+  end, { noremap = true, silent = true, desc = op_desc })
+  -- Visual mode: Operator
+  vim.keymap.set('x', 'ga' .. key, function()
+    require('textcase').operator(case)
+  end, { noremap = true, silent = true, desc = 'Convert to ' .. desc })
+end
+
+-- Define key mappings for various cases
+setup_textcase_keymaps('k', 'to_dash_case', 'kebab-case', 'to-kebab-case')
+setup_textcase_keymaps('d', 'to_dot_case', 'dot.case', 'to.dot.case')
+setup_textcase_keymaps('t', 'to_title_case', 'Title Case', 'To Title Case')
+setup_textcase_keymaps('/', 'to_path_case', 'path/case', 'to/path/case')
+setup_textcase_keymaps('<space>', 'to_phrase_case', 'phrase case', 'to phrase case')
